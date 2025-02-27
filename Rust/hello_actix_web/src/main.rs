6dd5d6f27975f7fcefa53f4ff9bce93c8e3fc33d
boss-kasai <kasaisfamily@gmail.com>
2025-02-27 17:00:02 +0900

    RustのAPIでhello worldを表示

---
+++ Rust/hello_actix_web/src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/health")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("Ok")
+}
+
+#[get("/hello")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, Actix-web!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(health_check)
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))? // ポート番号はお好みで
+    .run()
+    .await
+}
